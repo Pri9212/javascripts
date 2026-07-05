8f1e66866cc7b5620656ce0302225dd53b94558b
Priyanshu Yadav <yadavpriyanshu6528@gmail.com>
2026-07-05 11:44:50 +0530

    javascript

---
+++ app.js
@@ -0,0 +1,21 @@
+// let inp=document.querySelector("input");
+// inp.addEventListener("keydown",function(event){
+//    if(event.code=="keyA") {
+//     console.log("charecter moves forward");
+//    }else if(event.code=="keyB"){
+//     console.log("charcter moves backward");
+
+//    }else if(event.code=="keyU"){
+//     console.log("charecter moves upward");
+//    }else if(event.code=="keyD") {
+//     console.log("charecter moves downward")
+//    }
+// });
+let fr=document.querySelector("form");
+fr.addEventListener("submit",function(event){
+    event.preventDefault();
+let inp=document.querySelector("input");
+console.dir(inp);
+console.log(inp.value);
+    alert("form submitted");
+});
---
+++ app2.js
@@ -0,0 +1,16 @@
+//  const arryaverage=(arr)=>{
+// let sum=0;
+//  for(let number of arr){
+//     sum+=number;
+//  }
+//  return sum/arr.length;
+// }
+//  let arr=[1,2,3,4,5]
+// let s=arryaverage(arr);
+// console.log(s);
+let n=5;
+const isEven=(n)=>{
+    if(n%2==0) return true;
+    else return false;
+}
+console.log(isEven(n));
